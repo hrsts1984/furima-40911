d29017d780469cb2f50deaf74af7b945ea380864
hrsts1984 <harasa.rhythm@gmail.com>
2024-09-28 09:57:01 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item),permit(:category_id, :product_condition_id, :shipping_cost_id, :shipping_area_id, :shipping_time_id, :product_name, :price, :image).merge(user_id: current_user.id)
+  end
 end
