0cfddd7a1101274702866d02ca1351022eb08275
hrsts1984 <harasa.rhythm@gmail.com>
2024-10-04 21:28:57 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
 
     if @item.save
-      redirect_to item_path(@item), notice: 'Item was successfully created.'
+      redirect_to root_path, notice: 'Item was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
