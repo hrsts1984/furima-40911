bae39d00d0a682fff938bfc02ad3ce9f1e6b8abc
hrsts1984 <harasa.rhythm@gmail.com>
2024-10-06 23:58:46 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :find_item, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.all
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create]
   
   end
 
